7c9e450183515bded2074701aba25ab1b509c9f7
181700008 <181700008@LAB30813.senacinformatica.br>
2018-11-20 12:03:56 -0200

    alterado o MedicamentoDAO Tela de login

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Lista M171</title>
+    </head>
+    <body>
+        <?php
+        //chama o arquivo verificando se já não foi chamado antes
+        require_once 'menu.php';
+        ?>
+
+
+
+        <br><br><br><br>
+        <h1 align="center"> <i>  Bem-vindo ao Lista Compras </i> </h1>
+
+
+
+        <?php
+        ?>
+    </body>
+</html>
